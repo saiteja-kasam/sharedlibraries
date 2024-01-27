dded23e74f8db72e81a9b6e6b80e1c0e3d7d9705
saiteja-kasam <155877288+saiteja-kasam@users.noreply.github.com>
2024-01-27 11:45:07 +0530

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,21 @@
+def gitDownload(repo)
+{
+   git "https://github.com/intelliqittrainings/${repo}.git"
+}
+
+
+def mavenBuild()
+{
+     sh 'mvn package'
+}
+
+def tomcatDeploy(jobname,ip,context)
+{
+
+sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
+
+def executeSelenium(jobname)
+{
+   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
